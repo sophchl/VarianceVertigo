07cb825cebb83216081f479e69f22258ae26c833
Sophia <so9ch11@web.de>
2020-03-29 21:16:35 +0200

    try some more--
    but not matching still

--- data_reading_temp.py
+++ data_reading_temp.py
@@ -35,6 +35,9 @@ tb1 = tbill_raw1[:]
 tbill_raw2 = pd.read_csv('data/raw/3mtbill2.csv')
 tb2 = tbill_raw2[:]
 
+tbill_raw3 = pd.read_csv('data/raw/3mtbill3.csv')
+tb3 = tbill_raw3[:]
+
 #%% process/add tbill (tb1)
 
 print(tb1.columns)
@@ -49,10 +52,17 @@ print(tb2.columns)
 tb2.columns  = ['ident', 'date', 'bidytm', 'askytm', 'nomytm']
 tb2['date'] = pd.to_datetime(tb2['date'])
 
-tb2['rf'] = ((1+(tb2['nomytm'])/100)**(1/12))-1
-tb2['rfy'] = tb2['nomytm']/100
+tb2['rfm'] = ((1+(tb2.nomytm)/100)**(1/12))-1
+tb2['rfy'] = tb2.nomytm/100
 
-data_all = pd.merge(tb2[['date', 'rf', 'rfy']], data_all, on = 'date')
+data_all = pd.merge(tb2[['date', 'rfm', 'rfy']], data_all, on = 'date')
+
+#%% process/add tbill (tb3)
+
+tb3.columns = ['date', 'rfm', 'level']
+tb3['date']= pd.to_datetime(tb3['date'], format='%Y%m%d')
+
+#data_all = pd.merge(tb3[['date', 'rfm']], data_all, on = 'date')
 
 #%% read spx
 
@@ -61,13 +71,14 @@ spx = spx_raw[:]
 
 #%% process/add spx 
 
-spx.columns = ['date', 'value', 'count', 'level', 'return']
+spx.columns = ['date', 'value', 'count', 'level', 'rtrnm']
 spx['date'] = pd.to_datetime(spx['date'], format='%Y%m%d')
 
-spx['logrtn'] = np.log(spx.level) - np.log(spx.level.shift(1))
-spx['logrtny'] = spx['logrtn']*12
+spx['logrtnm'] = np.log(spx.level) - np.log(spx.level.shift(1))
+spx['logrtny'] = spx.logrtnm*12
+spx['rtrny'] = (1 + spx.rtrnm)**(12) - 1
 
-data_all = pd.merge(spx[['date', 'logrtn', 'logrtny']], data_all, on = 'date')
+data_all = pd.merge(spx[['date', 'rtrnm', 'rtrny', 'logrtnm', 'logrtny']], data_all, on = 'date')
 
 #%% read options
 
@@ -78,8 +89,22 @@ data_all = pd.merge(spx[['date', 'logrtn', 'logrtny']], data_all, on = 'date')
 
 #%% work on whole dataset
 
-data_all['excessm'] = data_all['logrtn'] - data_all['rf']
-data_all['excessy'] = data_all['logrtny'] - data_all['rfy']
+# difference of absolute return and rf and log afterwards
+data_all['excessm1'] = data_all.rtrnm - data_all.rfm
+data_all['logexcessm1'] = np.log(1 + data_all.excessm1)
+data_all['logexcessmy1'] = data_all.logexcessm1*12
+data_all['excessy1'] = data_all.rtrny - data_all.rfy
+data_all['logexcessy1'] = np.log(1 + data_all.excessy1)
+
+# difference of log return to absolutre risk free rate (though it does not make much sense)
+data_all['logexcessm2'] = data_all.logrtnm - data_all.rfm
+data_all['logexcessy2'] = data_all.logrtny - data_all.rfy
+ 
+# differnce of log return and log rf
+data_all['logexcessm3'] = data_all.logrtnm - np.log(1 + data_all.rfm)
+data_all['logexcessmy3'] = data_all.logexcessm3*12
+data_all['logexcessy3'] = data_all.logrtny - np.log(1 + data_all.rfy)
+
 
 data_rep = my_time_filter(data_all, start_date, end_date)
 print(data_rep.mean()*100)
