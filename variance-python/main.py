66652a6d4da8e19b42b8883b827c13c6b0c6c889
Sophia <so9ch11@web.de>
2020-04-06 10:37:06 +0200

    create python project
    add example module

---
+++ variance-python/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# this is our main python file, all functions we created come together
+
+#%% load dependencies
+
+import numpy as np
+import pandas as pd
+
+#%% example on how this works (source: https://docs.python.org/3/tutorial/modules.html)
+
+import fibo 
+# show what functions are in a module
+print(dir(fibo))
+# execute the function fib2, assign to fibonacci
+fibonacci = fibo.fib2(10)
+
+# more ressources on modules: https://docs.python.org/3/tutorial/modules.html
+    
+#%% import the data 
+
+#%% run the models
+
+#%% plto the results
+
+#%% robustness tests
+
+
+    
+    
+
+
+
+
+
