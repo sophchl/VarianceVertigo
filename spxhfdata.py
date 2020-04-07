255e370f63b5401ba21b9fcf35af72ddb8a952ce
Sophia <so9ch11@web.de>
2020-04-07 11:52:36 +0200

    add overnight return

--- spxhfdata.py
+++ spxhfdata.py
@@ -42,8 +42,7 @@ for file in os.listdir(data_directory):
 spx = pd.concat(list_dataframes)
 spx.columns = ['symbol', 'date', 'time', 'bid']
 spx = spx.drop(['symbol'], axis = 1)
-spx_datetime = spx['date'].astype(str) + ":" + spx['time']
-spx.index = pd.to_datetime(spx_datetime, format = '%Y%m%d:%H:%M:%S')
+spx.index = pd.to_datetime(spx['date'].astype(str) + ":" + spx['time'], format = '%Y%m%d:%H:%M:%S')
 spx['date'] = pd.to_datetime(spx['date'], format='%Y%m%d').dt.date
 spx['time'] = pd.to_datetime(spx['time'], format='%H:%M:%S').dt.time
 
@@ -58,15 +57,12 @@ day, night = separate_tradingday_overnight(spx)
 # A: selecting values 
 
 day1 = day.resample('1T').mean()
-
 fivemin = np.arange(dt.datetime(1996,9,30), dt.datetime(1999,12,2),
                           dt.timedelta(minutes = 5)).astype(dt.datetime)
 fivemin = pd.DataFrame(index = fivemin)
-
 fivemin_day = separate_tradingday_overnight(fivemin)[0]
 
 spx5a = pd.merge(day1, fivemin_day, right_index = True, left_index = True)
-day1, fivemin, fivemin_day = None
 
 # B: 5 min resample
 # looks like average of e.g. 8:30 are all values from 8:25 to 8:30 etc.
@@ -76,7 +72,7 @@ spx5b =  spx5b[1:]
 
 # continue with one of them
 
-mydata = spx5a[:]
+mydata = spx5b
 
 #%% average bid ask quotes
 
@@ -86,12 +82,18 @@ mydata = spx5a[:]
 
 mydata['return'] = (mydata.bid - mydata.bid.shift(1))/(mydata.bid)
 
+# create mydata2: aggregate to daily data
 mydata2 = (mydata[['return']].groupby(mydata.index.date).sum())**2
 mydata2.columns = ['rv']
 
 # add squared overnight return
+spx1 = spx.resample('1T').mean()
+mydata2['open'] = spx1[spx1.index.time == dt.time(9,31,0)].bid.resample('1D').sum()
+mydata2['close'] = spx1[spx1.index.time == dt.time(16,0,0)].bid.resample('1D').sum()
+mydata2['overnight'] = (mydata2['open'] - mydata2['close'])/mydata2['close']
+mydata2['rv2'] = mydata2['rv'] + mydata2['overnight']**2
 
-# what return do we take?
+# overnight return is huge compared to 5-min variances!!
 
 
 #%% calculate upside and downside realized variance
