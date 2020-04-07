173b1afe41696fc2a72cbe6682133debb7391bf3
Sophia <so9ch11@web.de>
2020-04-07 13:39:44 +0200

    upside and downside RV

--- spxhfdata.py
+++ spxhfdata.py
@@ -12,6 +12,10 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
+#%% set parameters
+
+kappa = 0
+
 #%% functions needed
 
 def separate_tradingday_overnight(data):
@@ -41,7 +45,6 @@ for file in os.listdir(data_directory):
 
 spx = pd.concat(list_dataframes)
 spx.columns = ['symbol', 'date', 'time', 'bid']
-spx = spx.drop(['symbol'], axis = 1)
 spx.index = pd.to_datetime(spx['date'].astype(str) + ":" + spx['time'], format = '%Y%m%d:%H:%M:%S')
 spx['date'] = pd.to_datetime(spx['date'], format='%Y%m%d').dt.date
 spx['time'] = pd.to_datetime(spx['time'], format='%H:%M:%S').dt.time
@@ -57,12 +60,11 @@ day, night = separate_tradingday_overnight(spx)
 # A: selecting values 
 
 day1 = day.resample('1T').mean()
-fivemin = np.arange(dt.datetime(1996,9,30), dt.datetime(1999,12,2),
+fivemin = np.arange(dt.datetime(1996,9,30), dt.datetime(1999,12,3),
                           dt.timedelta(minutes = 5)).astype(dt.datetime)
 fivemin = pd.DataFrame(index = fivemin)
-fivemin_day = separate_tradingday_overnight(fivemin)[0]
 
-spx5a = pd.merge(day1, fivemin_day, right_index = True, left_index = True)
+spx5a = pd.merge(day1, fivemin, right_index = True, left_index = True)
 
 # B: 5 min resample
 # looks like average of e.g. 8:30 are all values from 8:25 to 8:30 etc.
@@ -80,25 +82,32 @@ mydata = spx5b
 
 #%% calculate return and realized variances
 
-mydata['return'] = (mydata.bid - mydata.bid.shift(1))/(mydata.bid)
+mydata['rtrn'] = np.log(mydata.bid) - np.log(mydata.bid.shift(1))
 
-# create mydata2: aggregate to daily data
-mydata2 = (mydata[['return']].groupby(mydata.index.date).sum())**2
+# create mydata2: aggregate to rv daily data
+mydata2 = (mydata[['rtrn']]**2).resample('1D').sum()
 mydata2.columns = ['rv']
 
 # add squared overnight return
 spx1 = spx.resample('1T').mean()
-mydata2['open'] = spx1[spx1.index.time == dt.time(9,31,0)].bid.resample('1D').sum()
-mydata2['close'] = spx1[spx1.index.time == dt.time(16,0,0)].bid.resample('1D').sum()
-mydata2['overnight'] = (mydata2['open'] - mydata2['close'])/mydata2['close']
+mydata2['open'] = spx1[spx1.index.time == dt.time(9,31,0)].bid.resample('1D').mean()
+mydata2['close'] = spx1[spx1.index.time == dt.time(16,0,0)].bid.resample('1D').mean()
+mydata2['overnight'] = np.log(mydata2['open']) - np.log(mydata2['close'].shift(1))
 mydata2['rv2'] = mydata2['rv'] + mydata2['overnight']**2
-
-# overnight return is huge compared to 5-min variances!!
-
 
 #%% calculate upside and downside realized variance
 
+mydata2['rv_u'] = (mydata[mydata['rtrn'] > kappa]['rtrn']**2).resample('1D').sum()
+mydata2['rv_d'] = (mydata[mydata['rtrn'] <= kappa]['rtrn']**2).resample('1D').sum()
 
+# test
+print(False in round((mydata2['rv_u'] + mydata2['rv_d']),10) == round(mydata2['rv'],10))
+
+# add overnight 
+mydata2['rv_u2'] = mydata2['rv_u'] + mydata2[mydata2['overnight'] > kappa]['overnight']**2
+mydata2['rv_d2'] = mydata2['rv_d'] + mydata2[mydata2['overnight'] <= kappa]['overnight']**2
+
+mydata2 = mydata2.drop(['close', 'open', 'overnight'], axis = 1)
 
 #%% apply scaling
 
@@ -115,3 +124,4 @@ def get_fl_time(data, length):
         
 get_fl_time(day, 500)
 
+#(mydata[['rtrn']]**2).groupby(mydata.index.date).sum()
